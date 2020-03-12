1f0b9b2ab0448fa6f4bcff64d54614de34ef2fea
Paul Biron <paul@sparrowhawkcomputing.com>
2020-03-12 12:36:07 -0600

    Ensure "Automatic Updates" column is not added if no content would be output in the column.
    Be sure to keep the test here in sync with the early exit test in wp_autoupdates_add_plugins_autoupdates_column_content().
    
    Fixes #56

--- wp-autoupdates.php
+++ wp-autoupdates.php
@@ -87,6 +87,9 @@ add_filter( 'auto_update_plugin', 'wp_autoupdates_selected_plugins', 10, 2 );
  * Add autoupdate column to plugins screen.
  */
 function wp_autoupdates_add_plugins_autoupdates_column( $columns ) {
+	if ( ! current_user_can( 'update_plugins' ) || ! wp_autoupdates_is_plugins_auto_update_enabled() ) {
+		return $columns;
+	}
 	if ( ! isset( $_GET['plugin_status'] ) || ( 'mustuse' !== $_GET['plugin_status'] && 'dropins' !== $_GET['plugin_status'] ) ) {
 		$columns['autoupdates_column'] = __( 'Automatic updates', 'wp-autoupdates' );
 	}
